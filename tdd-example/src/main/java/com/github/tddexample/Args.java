64b64b1ebea4ba36b0638e1cd236ba15e805c806
hanchenhao <hanchenhao@vip.qq.com>
2023-03-17 15:53:03 +0800

    单一参数测试，及对应实现代码

--- tdd-example/src/main/java/com/github/tddexample/Args.java
+++ tdd-example/src/main/java/com/github/tddexample/Args.java
@@ -1,7 +1,32 @@
 package com.github.tddexample;
 
+import lombok.val;
+
+import java.util.Arrays;
+
 public class Args {
-    public static <T>T prase(Class<T> optionsClass, String... args) {
-        return null;
+    public static <T> T parse(Class<T> optionsClass, String... args) {
+        val constructor = optionsClass.getDeclaredConstructors()[0];
+        try {
+            val parameter = constructor.getParameters()[0];
+            val option = parameter.getAnnotation(Option.class);
+            val arguments = Arrays.asList(args);
+            Object value = null;
+
+            if (parameter.getType() == boolean.class) {
+                value = arguments.contains("-" + option.value());
+            }
+            if (parameter.getType() == int.class) {
+                int index = arguments.indexOf("-" + option.value());
+                value = arguments.size() == 1 ? 0 : Integer.parseInt(arguments.get(index + 1));
+            }
+            if (parameter.getType() == String.class) {
+                int index = arguments.indexOf("-" + option.value());
+                value = arguments.size() == 1 ? "" : String.valueOf(arguments.get(index + 1));
+            }
+            return (T) constructor.newInstance(value);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- tdd-example/src/test/java/com/github/tddexample/ArgsTest.java
+++ tdd-example/src/test/java/com/github/tddexample/ArgsTest.java
@@ -8,10 +8,54 @@ import org.junit.jupiter.api.Test;
 public class ArgsTest {
     // -l -p 8080 -d /usr/logs
     // happy path
-    // TODO 输入：-l， 输出：options.logging() = true
-    // TODO 输入：-p 8080，输出： options.port()=8080
+    // 输入：-l， 输出：options.logging() = true
+    @Test
+    public void should_get_boolean_option_to_true_if_flag_present(){
+        val option = Args.parse(BooleanOption.class, "-l");
+        Assertions.assertTrue(option.logging());
+    }
+    @Test
+    public void should_get_boolean_option_to_false_if_flag_present(){
+        val option = Args.parse(BooleanOption.class);
+        Assertions.assertFalse(option.logging());
+    }
+    static record BooleanOption(@Option("l")boolean logging){}
+    //  输入：-p 8080，输出： options.port()=8080
+
+    @Test
+    public void should_get_int_option_if_flag_present(){
+        val option = Args.parse(IntOption.class, "-p", "8080");
+        Assertions.assertEquals(8080,option.port());
+    }
+    @Test
+    public void should_get_zero_option_if_input_empty_value(){
+        val option = Args.parse(IntOption.class, "-p");
+        Assertions.assertEquals(0,option.port());
+    }
+    static record IntOption(@Option("p")int port){}
     // TODO 输入：-d /usr/logs，输出： options.directory()=/usr/logs
+    @Test
+    public void should_get_string_option_if_flag_present(){
+        val option = Args.parse(StringOption.class, "-d", "/usr/logs");
+        Assertions.assertEquals("/usr/logs",option.directory());
+    }
+
+    @Test
+    public void should_get_null_if_input_empty_value(){
+        val option = Args.parse(StringOption.class, "-d");
+        Assertions.assertEquals("",option.directory());
+    }
+    static record StringOption(@Option("d")String directory){}
     // TODO 输入：-l -p 8080 -d /usr/logs，输出：options（logging=true，port=8080，directory="/usr/logs"）
+    @Test
+    public void input_multiple_parameter_to_get_ture_option() {
+        // 输入-l -p 8080 -d /usr/logs，将内容进行分割，并提取参数
+        Options options = Args.parse(Options.class, "-l", "-p", "8080", "-d", "/usr/logs");
+        Assertions.assertTrue(options.logging());
+        Assertions.assertEquals(8008, options.port());
+        Assertions.assertEquals("/usr/logs", options.directory());
+
+    }
 
     //sad path
     // TODO 输入：-l t f，输出：异常提示
@@ -25,22 +69,13 @@ public class ArgsTest {
     // -g this is a list -d 1 2 -3 5
 
     // 根据标志位，把内容进行分割
-    @Test
-    @Disabled
-    public void example_1() {
-        // 输入-l -p 8080 -d /usr/logs，将内容进行分割，并提取参数
-        Options options = Args.prase(Options.class, "-l", "-p", "8080", "-d", "/usr/logs");
-        Assertions.assertTrue(options.logging());
-        Assertions.assertEquals(8008, options.port());
-        Assertions.assertEquals("/usr/logs", options.directory());
 
-    }
 
     @Test
     @Disabled
     public void example_2() {
         // 输入g this is a list -d 1 2 -3 5，将内容进行分割，并提取参数列表
-        val listOptions = Args.prase(ListOptions.class, "-g", "this", "is", "a", "list", "-d", "1", "2", "3");
+        val listOptions = Args.parse(ListOptions.class, "-g", "this", "is", "a", "list", "-d", "1", "2", "3");
         Assertions.assertEquals(new String[]{"this", "is", "a", "list"}, listOptions.group());
         Assertions.assertEquals(new int[]{1, 2, 3}, listOptions.decimals());
 
