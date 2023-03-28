a002f54a979b8b42c69c7d2c910fdea9686e56e1
hanchenhao <hanchenhao@vip.qq.com>
2023-03-28 11:53:30 +0800

    initial project

---
+++ TDD-DI/src/main/java/com/example/tdddi/TddDiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tdddi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddDiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddDiApplication.class, args);
+    }
+
+}
---
+++ TDD-DI/src/test/java/com/example/tdddi/TddDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tdddi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
